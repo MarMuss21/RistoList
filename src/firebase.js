5d61a5aa0845b4747e0996315adc6a8a5a3fae16
MarMuss21 <marcom_96@live.it>
2025-07-03 17:27:17 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,22 @@
-// firebase.js
+// src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDi3uCJyDp4i4Bc-zs-2Eg9OWbriWeL4KE",
+  apiKey: "AIzaSyDia3UCyD4p4i8Dc-zS-2Eg9OWbrWeL4KE",
   authDomain: "amici-di-risto.firebaseapp.com",
   projectId: "amici-di-risto",
   storageBucket: "amici-di-risto.appspot.com",
-  messagingSenderId: "580835556624",
-  appId: "1:580835556624:web:dad52d15351746b6064995",
-  measurementId: "G-84KMNSLRZD"
+  messagingSenderId: "508835556624",
+  appId: "1:508835556624:web:dad52d15351746b6b64995",
+  measurementId: "G-84KMNSLRZD",
 };
 
+// Inizializza Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inizializza l'autenticazione con Google
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
