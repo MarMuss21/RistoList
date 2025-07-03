712f001c99ee5f1cea5383bf605704d4b9225c9a
MarMuss21 <marcom_96@live.it>
2025-07-03 16:49:05 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDi3uCyD4p4iB0c-zS-2Eg90MbrWreL4KE",
+  apiKey: "AIzaSyDi3uCJyDp4i4Bc-zs-2Eg9OWbriWeL4KE",
   authDomain: "amici-di-risto.firebaseapp.com",
   projectId: "amici-di-risto",
   storageBucket: "amici-di-risto.appspot.com",
-  messagingSenderId: "508835556624",
-  appId: "1:508835556624:web:dad52d1531746b0604995",
+  messagingSenderId: "580835556624",
+  appId: "1:580835556624:web:dad52d15351746b6064995",
   measurementId: "G-84KMNSLRZD"
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+export { auth, provider };
