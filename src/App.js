dc0b8e74242e747c627c2d52ac6b9d41493b1f49
MarMuss21 <marcom_96@live.it>
2025-07-04 12:37:08 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GroupsList from "./components/GroupsList";
+
+function App() {
+  return (
+    <div>
+      <h1>RistoList</h1>
+      <GroupsList />
+    </div>
+  );
+}
+
+export default App;
